3e55a9b4364c50143672dc30d5bb37af855e9741
renukanandatd <renukananda2000@gmail.com>
2023-09-08 14:22:05 +0530

    Second button

--- app/src/main/java/com/example/mini_1/MainActivity.kt
+++ app/src/main/java/com/example/mini_1/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.mini_1
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+   lateinit var imageView : ImageView
+   lateinit var button: Button
+   val REQUEST_IMAGE_CAPTURE = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.button2)
+
+        button.setOnClickListener {
+
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try{
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
+
+        }
+        catch(e:ActivityNotFoundException){
+            Toast.makeText(this,"Error:" + e.localizedMessage,Toast.LENGTH_SHORT).show()
+
+        }        }
 
 
         var intentBtn:Button=findViewById<Button>(R.id.button1)
@@ -18,5 +41,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+        else{
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
     }
 }
